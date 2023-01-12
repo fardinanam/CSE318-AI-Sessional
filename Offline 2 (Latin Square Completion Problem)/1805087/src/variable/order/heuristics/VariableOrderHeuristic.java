b1d0362341244980e1bbd6795a7945b59dbe04ca
Fardin Anam Aungon <52667920+fardinanam@users.noreply.github.com>
2023-01-12 09:49:47 +0600

    Delete VariableOrderHeuristic.java

--- Offline 2 (Latin Square Completion Problem)/1805087/src/variable/order/heuristics/VariableOrderHeuristic.java
+++
@@ -1,8 +0,0 @@
-package variable.order.heuristics;
-
-import csps.Csp;
-import variables.Variable;
-
-public interface VariableOrderHeuristic <T, T1 extends Variable<T>, T2 extends Csp<T, T1>> {
-    public T1 getNextVariable(T2 csp);
-}
